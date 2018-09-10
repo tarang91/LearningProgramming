174bc294b433adff9cc9bf8dcf8bba6a0b9626bd
tarang91 <tarangs123@gmail.com>
2018-09-10 13:55:56 +0530

    Commit 4

--- src/test/java/LearningSelenium/LearningSelenium/TestClass.java
+++ src/test/java/LearningSelenium/LearningSelenium/TestClass.java
@@ -3,5 +3,6 @@ package LearningSelenium.LearningSelenium;
 public class TestClass {
 	public static void main (String args[]) {
 		System.out.println("This is my first java File!");
+		System.out.println("this is after commiting using git bash");
 	}
 }
